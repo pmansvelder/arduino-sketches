67240a989950ea579a42966b4adc409d2691f2b3
Peter Mansvelder <pman@mac.com>
2018-02-28 15:14:07 +0100

    sensor updates
    Modified for sensors and pulse relay

--- Domus_Mega_W5100_01/Domus_Mega_W5100_01.ino
+++ Domus_Mega_W5100_01/Domus_Mega_W5100_01.ino
@@ -1,18 +1,28 @@
 /*
- *        <========Arduino Sketch for Arduino Mega with Ethernet shield W5100=========>
- *        
+          <========Arduino Sketch for Arduino Mega with Ethernet shield W5100=========>
+
           Arduino UNO with W5100 Ethernetshield or W5100 Ethernet module, used as MQTT client
           It will connect over Wifi to the MQTT broker and controls digital outputs (LED, relays)
-          The topics have the format "domus_mqtt/up" for outgoing messages and  "domus_mqtt/down" for incoming messages
-          As the available memory of a UNO  with Ethernetcard is limited, I have kept the topics short
+          The topics have the format "domus/hk/uit" for outgoing messages and
+          "domus/hk/in" for incoming messages.
+          As the available memory of a UNO  with Ethernetcard is limited,
+          I have kept the topics short
           Also, the payloads  are kept short
           The outgoing topics are
-          /domus_mqtt/up
-          Here, eacht relay state is reported using the same syntax as the switch command:
+
+          domus/hk/uit        // Relaisuitgangen: R<relaisnummer><status>
+          domus/hk/uit/rook   // MQ-2 gas & rookmelder, geconverteerd naar 0-100%
+          domus/hk/uit/licht  // LDR-melder: 0=licht, 1=donker
+          domus/hk/uit/temp   // DHT-11 temperatuursensor
+          domus/hk/uit/warmte // DHT-11 gevoelstemperatuur
+          domus/hk/uit/vocht  // DHT-11 luchtvochtigheid
+          domus/ex/uit/deur   // Pulserelais t.b.v. deuropener
+
+          Here, each relay state is reported using the same syntax as the switch command:
           R<relay number><state>
 
           There is only one incoming topic:
-          /domus_mqtt/down
+          domus/hk/in
           The payload here determines the action:
           STAT - Report the status of all relays (0-9)
           AON - turn all the relays on
@@ -30,37 +40,79 @@
 
           Uno: pins 4,10,11,12,13 in use
           Mega: 4,10,50,51,52,53 in use
-          
+
           3,5,6,7,8,9,A0(14),A1(15),A2(16),A3(17), using those not used by ethernet shield (4, 10, 11, 12, 13) and other
           ports (0, 1 used by serial interface).
           A4(18) and A5(19) are used as inputs, for 2 buttons
 
           ToDo:
-          - add extra button on input A5(19)
-          - use output 17 as a PWM channel for the button LEDs
-          - implement short/long press for buttons
+          - add extra button on input A5(19) => done
+          - use output 17 as a PWM channel for the button LEDs => done, used A9
+          - implement short/long press for buttons => done
+
+          Adapted 24-02-2018 by Peter Mansvelder:
+          - added sensors for light and smoke (MQ-2), reporting on output topics
+          - added alarm function for smoke with buzzer
+          - added pulse relay
 
 */
+
 #include <Ethernet.h>// Ethernet.h library
 #include "PubSubClient.h" //PubSubClient.h Library from Knolleary
-#define CLIENT_ID       "SW1_huiskamer"
-#define PUBLISH_DELAY   3000 // that is 3 seconds interval
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+// Vul hier de pin in van de DHT11 sensor
+#define DHT_PIN 3
+DHT dht(DHT_PIN, DHT11);
+
+// Vul hier de naam in waarmee de Arduino zich aanmeldt bij MQTT
+#define CLIENT_ID  "domus_meterkast"
+
+// Vul hier het interval in waarmee sensorgegevens worden verstuurd op MQTT
+#define PUBLISH_DELAY  3000 // that is 3 seconds interval
 #define DEBOUNCE_DELAY 200
+#define LONGPRESS_TIME 450
+
+// Vul hier de tijd in voor het pulserelais
+#define PULSE_RELAY_TIME 2500
 
 String hostname = "domus_huiskamer";
+const char* topic_in = "domus/hk/in";
+const char* topic_out = "domus/hk/uit";
+const char* topic_out_smoke = "domus/hk/uit/rook";
+const char* topic_out_light = "domus/hk/uit/licht";
+const char* topic_out_door = "domus/ex/uit/deur";
+const char* topic_out_temp = "domus/hk/uit/temp";
+const char* topic_out_hum = "domus/hk/uit/vocht";
+const char* topic_out_heat = "domus/hk/uit/warmte";
 
-int NumberOfRelays = 10;
-int RelayPins[] = {9, 8, 7, 6, 5, 3, 14, 15, 16, 17};
+// Vul hier het aantal gebruikte relais in en de pinnen waaraan ze verbonden zijn
+int NumberOfRelays = 2;
+int RelayPins[] = {6, 7};
 
+// Vul hier het aantal knoppen in en de pinnen waaraan ze verbonden zijn
 int NumberOfButtons = 2;
-int ButtonPins[] = {18, 19};
+int ButtonPins[] = {8, 9};
 static byte lastButtonStates[] = {0, 0};
 long lastActivityTimes[] = {0, 0};
+long LongPressActive[] = {0, 0};
+
+// Vul hier de pin in van het pulserelais.
+int PulseRelayPin = 5;
+long PulseActivityTime = 0;
+
+// Vul hier de pin in van de rooksensor
+int SmokeSensor = A0;
+
+// Vul hier de pwm outputpin in voor de Ledverlichting van de knoppen
+int PWMoutput = 11; // Uno: 3, 5, 6, 9, 10, and 11, Mega: 2 - 13 and 44 - 46
+
+// Vul hier de pin in van de lichtsensor
+int LightSensor = 2;
 
 char messageBuffer[100];
 char topicBuffer[100];
-//char clientBuffer[50];
-
 String ip = "";
 bool statusKD = HIGH;
 bool statusBD = HIGH;
@@ -70,6 +122,8 @@ bool pir = LOW;
 bool startsend = HIGH;// flag for sending at startup
 bool debug = true;
 int lichtstatus; //contains LDR reading
+
+// Vul hier het macadres in
 uint8_t mac[6] = {0x00, 0x01, 0x02, 0x03, 0x04, 0x07};
 
 EthernetClient ethClient;
@@ -92,13 +146,20 @@ void setup() {
     pinMode(ButtonPins[thisButton], INPUT_PULLUP);
   }
 
+  dht.begin();
+
+  pinMode(SmokeSensor, INPUT);
+  pinMode(PWMoutput, OUTPUT);
+  pinMode(LightSensor, INPUT);
+  pinMode(PulseRelayPin, OUTPUT);
+
   // setup serial communication
 
   if (debug) {
     Serial.begin(9600);
     while (!Serial) {};
 
-    ShowDebug(F("MQTT Arduino Demo"));
+    ShowDebug(F("MQTT Arduino Domus"));
     ShowDebug("");
   }
   // setup ethernet communication using DHCP
@@ -131,72 +192,112 @@ void setup() {
   ShowDebug("");
   ShowDebug(F("Ready to send data"));
   previousMillis = millis();
-  mqttClient.publish("/domus_mqtt/up", ip.c_str());
-}
-
-void loop() {
-
-  // it's time to send new data?
-  if (millis() - previousMillis > PUBLISH_DELAY) {
-    sendData();
-    previousMillis = millis();
-  }
-  else {
- //   processButtonDigital(ButtonPin);
-    for (int id; id < NumberOfButtons; id++) {
-      processButtonDigital(id);
-    }
-  }
-  mqttClient.loop();
+  //  mqttClient.publish(topic_out, ip.c_str());
 }
 
 void processButtonDigital( int buttonId )
 {
   int sensorReading = digitalRead( ButtonPins[buttonId] );
-
-  if ( sensorReading == 0 ) // Input pulled low to GND. Button pressed.
+  if ( sensorReading == LOW ) // Input pulled low to GND. Button pressed.
   {
-
-    if ( lastButtonStates[buttonId] == 0 )  // The button was previously un-pressed
+    if ( lastButtonStates[buttonId] == LOW )  // The button was previously un-pressed
     {
       if ((millis() - lastActivityTimes[buttonId]) > DEBOUNCE_DELAY) // Proceed if we haven't seen a recent event on this button
       {
         lastActivityTimes[buttonId] = millis();
-        Serial.println( "Button" + String(buttonId) + " pressed" );
-        String messageString = "Button" + String(buttonId);
-        messageString.toCharArray(messageBuffer, messageString.length() + 1);
-        mqttClient.publish("/domus_mqtt/up", messageBuffer);
       }
+    }
+    else if ((millis() - lastActivityTimes[buttonId] > LONGPRESS_TIME) && (LongPressActive[buttonId] == false))// Button long press
+    {
+      LongPressActive[buttonId] = true;
+      ShowDebug( "Button" + String(buttonId) + " long pressed" );
+      String messageString = "Button" + String(buttonId) + "_long";
+      messageString.toCharArray(messageBuffer, messageString.length() + 1);
+      mqttClient.publish(topic_out, messageBuffer);
     }
     lastButtonStates[buttonId] = 1;
   }
   else {
-    lastButtonStates[buttonId] = 0;
+    if (lastButtonStates[buttonId] == true) {
+      if (LongPressActive[buttonId] == true) {
+        LongPressActive[buttonId] = false;
+      } else {
+        if ((millis() - lastActivityTimes[buttonId]) > DEBOUNCE_DELAY) // Proceed if we haven't seen a recent event on this button
+        {
+          ShowDebug( "Button" + String(buttonId) + " pressed" );
+          String messageString = "Button" + String(buttonId);
+          messageString.toCharArray(messageBuffer, messageString.length() + 1);
+          mqttClient.publish(topic_out, messageBuffer);
+        }
+      }
+      lastButtonStates[buttonId] = false;
+    }
+  }
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!mqttClient.connected()) {
+    ShowDebug("Attempting MQTT connection...");
+    // Attempt to connect
+    if (mqttClient.connect(CLIENT_ID)) {
+      ShowDebug("connected");
+      // Once connected, publish an announcement...
+      mqttClient.publish(topic_out, ip.c_str());
+      mqttClient.publish(topic_out, "hello world");
+      // ... and resubscribe
+      mqttClient.subscribe(topic_in);
+    } else {
+      ShowDebug("failed, rc=" + String(mqttClient.state()));
+      ShowDebug(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
   }
 }
 
 void sendData() {
-  char msgBuffer[20];
-  ShowDebug("%");
-  ShowDebug("Relay is: ");
-  ShowDebug((relaystate1 == LOW) ? "OPEN" : "CLOSED");
-  if (mqttClient.connect(CLIENT_ID)) {
-    mqttClient.subscribe("/domus_mqtt/down");
-    if (startsend) {
-      mqttClient.publish("/domus_mqtt/up", ip.c_str());
-      startsend = LOW;
-    }
-  }
+  int smoke = analogRead(SmokeSensor);
+  bool light = digitalRead(LightSensor);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  // Send Temperature sensor
+  String messageString = String(t);
+  messageString.toCharArray(messageBuffer, messageString.length() + 1);
+  mqttClient.publish(topic_out_temp, messageBuffer);
+
+  // Send Humidity sensor
+  messageString = String(h);
+  messageString.toCharArray(messageBuffer, messageString.length() + 1);
+  mqttClient.publish(topic_out_hum, messageBuffer);
+
+  // Send Heat index sensor
+  messageString = String(hic);
+  messageString.toCharArray(messageBuffer, messageString.length() + 1);
+  mqttClient.publish(topic_out_heat, messageBuffer);
+
+  // Send smoke sensor
+  messageString = String(map(smoke, 0, 1023, 0, 100));
+  messageString.toCharArray(messageBuffer, messageString.length() + 1);
+  mqttClient.publish(topic_out_smoke, messageBuffer);
+
+  // Send light sensor
+  messageString = String(light);
+  messageString.toCharArray(messageBuffer, messageString.length() + 1);
+  mqttClient.publish(topic_out_light, messageBuffer);
 }
 
 void report_state(int outputport)
 {
   String messageString = "R" + String(outputport) + String(digitalRead(RelayPins[outputport]));
   messageString.toCharArray(messageBuffer, messageString.length() + 1);
-  mqttClient.publish("/domus_mqtt/up", messageBuffer);
+  mqttClient.publish(topic_out, messageBuffer);
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char* topic, byte * payload, unsigned int length) {
   char msgBuffer[20];
   // I am only using one ascii character as command, so do not need to take an entire word as payload
   // However, if you want to send full word commands, uncomment the next line and use for string comparison
@@ -211,7 +312,9 @@ void callback(char* topic, byte* payload, unsigned int length) {
   int RelayValue;
 
   if (strPayload[0] == 'R') {
-    ShowDebug("Extended command");
+
+    // Relais commando
+    ShowDebug("Relay command");
 
     RelayPort = strPayload[1] - 48;
     if (RelayPort > 16) RelayPort -= 3;
@@ -222,39 +325,87 @@ void callback(char* topic, byte* payload, unsigned int length) {
     ShowDebug(String(HIGH));
 
     if (RelayValue == 40) {
-      ShowDebug("Toggling...");
+      ShowDebug("Toggling relay...");
       digitalWrite(RelayPins[RelayPort], !digitalRead(RelayPins[RelayPort]));
     } else {
       digitalWrite(RelayPins[RelayPort], RelayValue);
     }
     report_state(RelayPort);
-  } else if (payload[0] == 50)
-  {
-    mqttClient.publish("/domus_mqtt/up", ip.c_str());// publish IP nr
+  } else if (strPayload == "IP")  {
+    
+    // 'Show IP' commando
+    mqttClient.publish(topic_out, ip.c_str());// publish IP nr
   }
-  else if (strPayload == "AON")
-  {
+  else if (strPayload == "AON") {
+
+    // Alle relais aan
     for (int i = 0 ; i < NumberOfRelays; i++) {
       digitalWrite(RelayPins[i], 1);
       report_state(i);
     }
   }
-  else if (strPayload == "AOF")
-  {
+
+  else if (strPayload == "AOF") {
+
+    // Alle relais uit
     for (int i = 0 ; i < NumberOfRelays; i++) {
       digitalWrite(RelayPins[i], 0);
       report_state(i);
     }
   }
-  else if (strPayload == "STAT")
-  {
+  else if (strPayload == "STAT") {
+
+    // Status van alle relais
     for (int thisPin = 0; thisPin < NumberOfRelays; thisPin++) {
       report_state(thisPin);
     }
   }
+  else if (strPayload == "P01") {
+
+    // Pulserelais aan
+    ShowDebug("Enabling pulse relay.");
+    digitalWrite(PulseRelayPin, LOW);
+    String messageString = "P01";
+    messageString.toCharArray(messageBuffer, messageString.length() + 1);
+    mqttClient.publish(topic_out_door, messageBuffer);
+    PulseActivityTime = millis();
+    ShowDebug(String(PulseActivityTime));
+  }
+  else if (strPayload[0] == 'L') {
+    analogWrite(PWMoutput, strPayload.substring(1).toInt());
+  }
   else {
+
+    // Onbekend commando
     ShowDebug("Unknown value");
-    mqttClient.publish("/domus_mqtt/up", "Unknown command");
+    mqttClient.publish(topic_out, "Unknown command");
+  }
+}
+
+void loop() {
+  if (!mqttClient.connected()) {
+    ShowDebug("Not Connected!");
+    reconnect();
   }
 
+  if (((millis() - PulseActivityTime) > PULSE_RELAY_TIME) && digitalRead(PulseRelayPin) == LOW)
+  {
+    ShowDebug("Disabling pulse relay.");
+    ShowDebug(String(PulseActivityTime));
+    String messageString = "P00";
+    messageString.toCharArray(messageBuffer, messageString.length() + 1);
+    mqttClient.publish(topic_out_door, messageBuffer);
+    digitalWrite(PulseRelayPin, HIGH);
+  }
+  // it's time to send new data?
+  if (millis() - previousMillis > PUBLISH_DELAY) {
+    previousMillis = millis();
+    sendData();
+  }
+  else {
+    for (int id; id < NumberOfButtons; id++) {
+      processButtonDigital(id);
+    }
+  }
+  mqttClient.loop();
 }
