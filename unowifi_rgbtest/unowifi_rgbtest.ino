a8ba5d41145bf86c25c830aa379159a17f00e026
Peter Mansvelder <pman@mac.com>
2018-12-27 00:00:09 +0100

    Arduino Uno Wifi

---
+++ unowifi_rgbtest/unowifi_rgbtest.ino
@@ -0,0 +1,15 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(25, OUTPUT);
+  pinMode(26, OUTPUT);
+  pinMode(27, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(13, 1);
+//  digitalWrite(25, 1);
+//  digitalWrite(26, 1);
+//  digitalWrite(27, 1);
+}
